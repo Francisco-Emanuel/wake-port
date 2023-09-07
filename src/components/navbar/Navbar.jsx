f8ec7e4da400cc7a5b8bd0437481656b90fb0081
Francisco_Emanuel <chico_emanuelsoares@hotmail.com>
2023-09-07 16:31:50 -0300

    qrcode

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
-          <img src={Logo} alt="logo" />
+          <img src={Logo} alt="logo" id="logo" />
         </div>
         <div className="gpt3__navbar-links_container">
           <Menu />
