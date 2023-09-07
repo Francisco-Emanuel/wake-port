7d4b7899469ab62d4b5cb1622db64242ebd1136e
Francisco_Emanuel <chico_emanuelsoares@hotmail.com>
2023-09-07 18:09:48 -0300

    .

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -3,7 +3,7 @@ import './footer.css'
 const Footer = () => {
   return (
     <div className="smart__footer">
-        <p>© smart result</p>
+        <p>©smart result</p>
         <p>todos os direitos reservados</p>
     </div>
   )
