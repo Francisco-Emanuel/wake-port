923b5ffec5e1921fbab08e434f18109541ae0e85
Francisco Emanuel <chico_emanuelsoares@hotmail.com>
2024-03-03 23:55:25 -0300

    test

--- src/containers/jobs/Jobs.jsx
+++ src/containers/jobs/Jobs.jsx
@@ -7,11 +7,11 @@ const Jobs = () => {
       <h1>O que a <b>Wake</b> ja fez</h1>
       <p>Conheça os nossos trabalhos anteriores</p>
       <div className="wake__jobs-cards">
-        <Job type="identidade visual" img="/assets/capapizza.png" />
-        <Job type="website" img="/assets/capasite2.png" />
-        <Job type="identidade visual" img="/assets/capahamburguer.png" />
-        <Job type="website" img="/assets/capasite1.png" />
-        <Job type="social media" img="/assets/port-socialmedia.png" />
+        <Job type="identidade visual" img="/src/assets/capapizza.png" />
+        <Job type="website" img="/src/assets/capasite2.png" />
+        <Job type="identidade visual" img="/src/assets/capahamburguer.png" />
+        <Job type="website" img="/src/assets/capasite1.png" />
+        <Job type="social media" img="/src/assets/port-socialmedia.png" />
         <Job type="identidade visual" img="/src/assets/capaffy.png" />
       </div>
     </section>
